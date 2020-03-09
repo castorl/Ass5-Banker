4cc9498ba86a61593e34360c2d09c230e08b4396
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2020-03-08 20:08:52 -0700

    Update account.h

--- account.h
+++ account.h
@@ -1,8 +1,8 @@
 //
 // Created by <Name> on <Date>.
 //
-//Types of funds 
-//0: Money Market
+// Types of funds
+// 0: Money Market
 /*1: Prime Money Market
   2: Long-Term Bond
   3: Short-Term Bond
@@ -11,72 +11,82 @@
   6: Growth Equity Fund
   7: Growth Index Fund
   8: Value Fund
-  9: Value Stock Index*/ 
+  9: Value Stock Index*/
 
 #pragma once
 
+#include <array>
+#include <iostream>
 #include <ostream>
-#include <iostream> 
-#include <string> 
+#include <vector>
+#include <string>
 
-using namespace std; 
+using namespace std;
 
+const int MAX = 10;
 class Account {
-  public:
-  // displays accounts first name, last name, account ID, and balances in each fund
-  friend ostream& operator<<(ostream& out, const Account& other);
-  //default constructor
-  //Should we maybe make it explicit?
-  Account();
+    // displays accounts first name, last name, account ID, and balances in each
+    // fund
+    friend ostream& operator<<(ostream& out, const Account& other);
 
-  //creates account with first, last, and accID
-	Account(string lastName, string firstName, int accID);
+public:
+    // creates account with first, last, and accID
+    explicit Account(string lastName, string firstName, int accID);
 
-  // destructor
-	virtual ~Account();
-	
-  //opens accounts 
-  bool openAccount(stringstream& stream); 
+    // destructor
+    virtual ~Account();
 
-  //deposits amount into fund type of account
-	bool deposit(int amt, Funds type); 
-	
-  //transfers amount into one fund type to another of the same account or other
-	bool transfer(Account* other, int amt, Funds type, Funds oType); 
 
-  //withdraws ammunt from fund type
-  bool withdraw(int amt, Funds type);
+    // deposits amount into fund type of account
+    bool deposit(int amt, int type);
 
-  //dipslays transaction history 
-  void displayTrans(Funds fund) const; 
+    // withdraws amount from fund type
+    bool withdraw(int amt, int type);
 
-  //int getAccID() const; 
-  
-  //string getLast() const; 
+    // transfers amount into one fund type to another of the same account or
+    // other
+    bool transfer(Account* other, int amt, int type, int oType);
 
-  //string getFirst() const; 
+    // dipslays transaction history
+    void displayTrans(int fund) const;
 
-  private:
-  //name of account
-  string firstName;
-	string lastName;
+    //returns ID number
+    int getID() const;
 
-  //account id number that links with the name of the account
-  //1000<= ID <= 9999
-  int accID;
+    //returns first name
+    string getFirst() const;
 
-  enum Funds{
-      MoneyMarket,
-      PrimeMoneyMarket,
-      LongTermBond,
-      ShortTermBond,
-      IndexFund,
-      CapitalValueFund,
-      GrowthEquityFund,
-      GrowthIndexFund,
-      ValueFund,
-      ValueStockIndex
-  };
+    //returns last name
+    string getLast() const;
+
+    //returns balance
+    int getBalance(int fund) const;
+
+    //helper function to execute exception
+    bool withdrawExcerption(int amt, int fund, int fund2, int extra);
+
+private:
+    // Fund has its own properties
+    struct Fund {
+        // name of fun
+        string fundName;
+        // balance for specific fund
+        int balance;
+        // list to record transactions for that fund
+        vector<string> fundRecord;
+    };
+
+    // array of funds to keep track of everything
+    Fund acctFunds[MAX];
+
+    // name of account
+    string firstName;
+    string lastName;
+
+    // account id number that links with the name of the account
+    // 1000<= ID <= 9999
+    int accID;
+
+    // helper function that records transcations
+    void recordTrans(string& transaction, int num);
 };
-
-
