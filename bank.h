598bc0714bd1f7c402474e0584adc0f7f010c4ab
castorl <30878574+castorl@users.noreply.github.com>
2020-03-09 09:50:30 -0700

    Add files via upload

--- bank.h
+++ bank.h
@@ -1,68 +1,57 @@
 
 //
-// Created by <Name> on <Date>. 
+// Created by Melanie Alexandra Palomino & Alana Yao on March 6, 2020.
 //
 /* Banker has three phases
-// Phase 1: Read txt and input to queue 
-// Phase 2: Runs every transaction 
-// Phase 3: Displays all open accounts and balances in these accounts 
+// Phase 1: Read txt and input to queue
+// Phase 2: Runs every transaction
+// Phase 3: Displays all open accounts and balances in these accounts
 */
 
 #pragma once
 
 #include "accounttree.h"
 #include <fstream>
+#include <queue>
 #include <string>
-#include <queue> 
 
 using namespace std;
 
 class Bank {
   public:
-    // constructor for bank 
+    // constructor for bank
     Bank();
-    // destructor for bank 
+    // destructor for bank
     ~Bank();
-    
-    
+
     // calls helper functions to process transactions
     void processTransactions(const string& fileName);
 
     // helper function to add transactions to queue
-    void addToQueue(const string& fileName); 
+    void addToQueue(const string& fileName);
 
-    // displays accounts' fund balances 
+    // displays accounts' fund balances
     void displayAllBankBalances() const;
 
-
   private:
-    
     AccountTree accounts;
 
-    struct Action{
-      char transType; 
-      int acctTo; 
-      int acctFrom;
-      int amount; 
-      string first; 
-      string last; 
-      int fundTo; 
-      int fundFrom;
+    struct Action {
+        char transType;
+        // maybe change to acctOf to avoid confusion
+        int acctTo;
+        int acctFrom;
+        int amount;
+        string first;
+        string last;
+        int fundTo;
+        int fundFrom;
     };
 
-    queue <Action> transactionsQ;
-
-    // helper function to set transaction type
-    void transClassification();
+    queue<Action> transactionsQ;
 
     // helper function to run transactions
-    // use the switch case for transactions of either opening, depositing, 
-    // withdrawing, transferring or displaying to the correct account 
+    // use the switch case for transactions of either opening, depositing,
+    // withdrawing, transferring or displaying to the correct account
     void processQueue();
-
-    // function helper to determine the type of transaction 
-    // void transactionType(const string& transaction, char type);
-
-    //helper function to check input data
-    // bool checkData(stringstream& s,int& amt, int& fund, int& ID, int& ID2, int& fund2); 
 };
--- main.cpp
+++ main.cpp
@@ -1,35 +1,30 @@
 //
-// Created by <Name> on <Date>.
+// Created by Melanie Alexandra Palomino & Alana Yao on March 13, 2020.
 //
 
 #include "bank.h"
 #include <iostream>
 #include <string>
-#include <stdexcept>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  Bank bank;
+    Bank bank;
 
-  for (int i = 1; i < argc; i++) {
-    cout << "The command line argument(s) was " << argv[i] << endl;
-    string fileName = argv[i];
-    
-    bool fileExists = false;
-    while(!fileExists) {
-      try {
-        bank.processTransactions(fileName);
-        fileExists = true;
-      } catch (const invalid_argument& e) {
-        cout << e << endl;
-      }
+    for (int i = 1; i < argc; i++) {
+        cout << "The command line argument(s) was " << argv[i] << endl;
+        string fileName = argv[i];
+
+        try {
+            bank.processTransactions(fileName);
+        } catch (const invalid_argument& e) {
+            cout << fileName << "could not be found." << endl;
+        }
+
+        bank.displayAllBankBalances();
     }
-  }
-  
-  displayAllBankBalances();
-  
-  cout << "Done" << endl;
 
-  return 0;
+    cout << "Done" << endl;
+
+    return 0;
 }
