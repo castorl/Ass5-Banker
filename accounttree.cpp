1892679381483f70eb2abebc34a8c8e43c3d6d4c
castorl <30878574+castorl@users.noreply.github.com>
2020-03-09 17:13:15 -0700

    Add files via upload

--- accounttree.cpp
+++ accounttree.cpp
@@ -1,134 +1,132 @@
 //
-// Created by <Name> on <Date>.
+// Created by Melanie Alexandra Palomino & Alana Yao on March 6, 2020.
 //
 
 #include "accounttree.h"
 
+using namespace std;
 
-
-//constructor
-AccountTree::AccountTree(){}
+// constructor
+AccountTree::AccountTree() = default;
 
 // Delete all nodes in BST
-AccountTree::~AccountTree(){
-    clear();
-}
+AccountTree::~AccountTree() { clear(); }
 
 // Insert new account, returns true if successful
-//ordered by ID number.
+// ordered by ID number.
 bool AccountTree::insert(Account* account) {
-    //check if root is null
-    if(root == nullptr){
+    // check if tree is empty
+    if (isEmpty()) {
         root = new Node(account);
         return true;
     }
-        return insertRecursive(root,account);
+    return insertRecursive(root, account);
+}
+
+// recursive insert recursive helper
+// if lower than root go left, higher go right
+bool AccountTree::insertRecursive(Node* curr, Account* account) {
+    // less go left
+    if (account->getID() < curr->getAccount()->getID()) {
+        // base case for recursion
+        if (curr->getLeft() == nullptr) { // if left ptr is null(so leaf)
+            Node* newNodeAccount = new Node(account);
+            curr->setLeft(newNodeAccount);
+            return true;
+        }
+        return insertRecursive(curr->getLeft(), account);
+    } 
+    if (account->getID() > curr->getAccount()->getID()) {
+        // base case for recursion
+        if (curr->getRight() == nullptr) { // if right ptr is null(so leaf)
+            Node* newNodeAccount = new Node(account);
+            curr->setRight(newNodeAccount);
+            return true;
+        }
+        return insertRecursive(curr->getRight(), account);
+    }
+    // greater go right
+    return false;
 }
 
 // Retrieve account
 // returns true if successful AND *account points to account
-bool AccountTree::retrieve(const int& accountNumber, Account*& account) const {
-    //starts at root and calls recursive that does binary search
-    if(root != nullptr){
-        return retrieveRecursive(root,accountNumber,account);
-
+bool AccountTree::retrieve(const int& accountNumber, Account*& account) {
+    // starts at root and calls recursive that does binary search
+    if (!isEmpty()) {
+        return retrieveRecursive(root, accountNumber, account);
     }
+    return false;
+}
+
+// recursive retrieve helper
+bool AccountTree::retrieveRecursive(Node* curr, const int& acctID,
+                                    Account*& account) {
+    // start at root and search binary
+    if (curr != nullptr) {
+        if (curr->getAccount()->getID() == acctID) {
+            account = curr->getAccount();
+            return true;
+        }
+        if (acctID < curr->getAccount()->getID()) {
+            Node* nextNode = curr->getLeft();
+            return retrieveRecursive(nextNode, acctID, account);
+        }
+        if (acctID > curr->getAccount()->getID()) {
+            Node* nextNode = curr->getRight();
+            return retrieveRecursive(nextNode, acctID, account);
+        }
+    }
+    account = nullptr;
     return false;
 }
 
 // Display information on all accounts
 void AccountTree::display() const {
-    if(root == nullptr){
-        cout << "Error in display"; //debug purpose
+    if (root == nullptr) {
+        cout << "Error in display"; // debug purpose
     }
-    //call recursive
+    // call recursive
     displayR(root);
+}
+
+// recursive display function helper
+void AccountTree::displayR(Node* curr) const {
+    // display from top and go on
+    // base case
+    if (curr != nullptr) {
+        // once all left are display stops recursion
+        displayR(curr->getLeft());
+        cout << *curr->getAccount() << endl; // derefence account
+        // once it gets to right leaf stops recursion
+        displayR(curr->getRight());
+    }
 }
 
 // delete all information in AccountTree
 void AccountTree::clear() {
     clearR(root);
-    //sets root to nullptr
+    // sets root to nullptr
     root = nullptr;
 }
 
-// check if tree is empty
-bool AccountTree::isEmpty() const {
-    //if there is no root, then there is nothing
-    return root == nullptr;
-}
-
-//recursive insert recursive helper
-//if lower than root go left, higher go right
-bool AccountTree::insertRecursive(Node* curr, Account* account){
-    //less go left
-    if(account->getID() < curr-> getAccount()-> getID()){
-        //base case for recursion
-        if(curr->getLeft() == nullptr){//if left ptr is null(so leaf)
-            Node* newNodeAccount = new Node(account);
-            curr->setLeft(newNodeAccount);
-            return true;
-        }
-        return insertRecursive(curr->getLeft(),account);
-
-    }
-    if(account->getID() > curr-> getAccount()-> getID()){
-        //base case for recursion
-        if(curr->getRight() == nullptr){//if right ptr is null(so leaf)
-            Node* newNodeAccount = new Node(account);
-            curr->setRight(newNodeAccount);
-            return true;
-        }
-        return insertRecursive(curr->getRight(),account);
-
-    }
-    //greater go right
-    return false;
-}
-
-//recursive clear function helper
-void AccountTree::clearR(Node* curr){
-    //recursion stops when all accounts are deleted
-    if(curr != nullptr){
-        //go all the way to left leaf
-        //delete and come back up
+// recursive clear function helper
+void AccountTree::clearR(Node* curr) {
+    // recursion stops when all accounts are deleted
+    if (curr != nullptr) {
+        // go all the way to left leaf
+        // delete and come back up
         clearR(curr->getLeft());
-        //go all the way to right leaf
-        //delete and come back up
+        // go all the way to right leaf
+        // delete and come back up
         clearR(curr->getRight());
         delete curr->getAccount();
         delete curr;
     }
 }
 
-//recursive retrieve helper
-bool AccountTree::retrieveRecursive(Node* curr,const int& acctID, Account*& account) const{
-    //start at root and search binary
-    //base case
-    if(curr->getAccount()->getID() == acctID){
-        account = curr->getAccount();
-        return true; 
-    }
-    //if account is less go right
-    if(curr->getAccount()->getID() < account->getID()){
-        return retrieveRecursive(curr->getLeft(),acctID,account);
-    }
-    //greater go left
-    if(curr->getAccount()->getID() > account->getID()){
-        return retrieveRecursive(curr->getRight(),acctID,account);
-    }
-    return false;
-}
-
-//recursive display function helper
-void AccountTree::displayR(Node* curr) const{
-    //display from top and go on
-    //base case
-    if(curr != nullptr){
-        //once all left are display stops recursion
-        displayR(curr->getLeft());
-        cout << *curr->getAccount() << endl; //derefence account
-        //once it gets to right leaf stops recursion
-        displayR(curr->getRight());
-    }
+// check if tree is empty
+bool AccountTree::isEmpty() const {
+    // if there is no root, then there is nothing
+    return root == nullptr;
 }
--- bank.cpp
+++ bank.cpp
@@ -1,11 +1,11 @@
 //
-// Created by <Name> on <Date>.
+// Created by Melanie Alexandra Palomino & Alana Yao on March 6, 2020.
 //
 /* Banker has three phases
-// Phase 1: Read txt and input to queue 
-// Phase 2: Runs every transaction 
-//Banker also must print out errors to file 
-// Phase 3: Displays all open accounts and balances in these accounts 
+// Phase 1: Read txt and input to queue
+// Phase 2: Runs every transaction
+//Banker also must print out errors to file
+// Phase 3: Displays all open accounts and balances in these accounts
 */
 
 /*TO DO:
@@ -14,235 +14,244 @@
  *
  *
  * */
-#include "accounttree.h"
-#include "account.h"
 #include "bank.h"
-#include <queue>
-#include <stdlib.h>
-#include <string>
-#include <iostream>
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
+#include <queue>
 #include <sstream>
 #include <stdexcept>
+#include <string>
 
-using namespace std; 
+using namespace std;
 
-// constuctor for bank 
-Bank::Bank(){}
+// constuctor for bank
+Bank::Bank() = default;
 
-// destructor for bank 
-Bank::~Bank(){
-  //nothing to delete
+// destructor for bank
+Bank::~Bank() = default;
+// nothing to delete
+
+// process file's lines as transactions and add to queue
+void Bank::addToQueue(const string& fileName) {
+    // create file reader
+    ifstream inFile;
+    inFile.open(fileName);
+
+    string line;
+    char type;
+    string lastName, firstName;
+    int acctNum, acctNum2, amt, fund, fund2;
+    // while not end of file
+    // add transactions to queue
+    while (!inFile.eof()) {
+        getline(inFile, line); // check first letter
+        stringstream s(line);
+        s >> type;
+
+        switch (type) {
+        case 'O': // open account transaction
+        {
+            s >> lastName >> firstName >> acctNum;
+
+            Action newAcct;
+            newAcct.TransType = type;
+            newAcct.Last = lastName;
+            newAcct.First = firstName;
+            newAcct.AcctTo = acctNum;
+
+            transactionsQ.push(newAcct);
+            break;
+        }
+        case 'D': // deposit transaction
+        {
+            s >> acctNum >> amt;
+            fund = acctNum % 10;
+            acctNum = acctNum / 10;
+
+            Action deposit;
+            deposit.TransType = type;
+            deposit.AcctTo = acctNum;
+            deposit.Amount = amt;
+            deposit.FundTo = fund;
+
+            transactionsQ.push(deposit);
+            break;
+        }
+        case 'W': // withdraw transaction
+        {
+            s >> acctNum >> amt;
+            fund = acctNum % 10;
+            acctNum = acctNum / 10;
+
+            Action withdraw;
+            withdraw.TransType = type;
+            withdraw.AcctFrom = acctNum;
+            withdraw.Amount = amt;
+            withdraw.FundFrom = fund;
+
+            transactionsQ.push(withdraw);
+            break;
+        }
+        case 'T': // transfer transaction
+        {
+            s >> acctNum >> amt >> acctNum2;
+            fund = acctNum % 10;
+            acctNum = acctNum / 10;
+            fund2 = acctNum2 % 10;
+            acctNum2 = acctNum2 / 10;
+
+            Action transfer;
+            transfer.TransType = type;
+            transfer.AcctTo = acctNum2;
+            transfer.AcctFrom = acctNum;
+            transfer.Amount = amt;
+            transfer.FundFrom = fund;
+            transfer.FundTo = fund2;
+
+            transactionsQ.push(transfer);
+            break;
+        }
+        case 'H': // display history
+        {
+            s >> acctNum;
+            if (acctNum > 9999) { // display history of specific fund
+                fund = acctNum % 10;
+                acctNum = acctNum / 10;
+            } else {       // display history of whole account
+                fund = -1; // no fund specified
+            }
+            Action history;
+            history.TransType = type;
+            history.AcctFrom = acctNum;
+            history.FundFrom = fund;
+
+            transactionsQ.push(history);
+            break;
+        }
+        default: {
+            cout << "Invalid transaction type.";
+            break;
+        }
+        }
+    }
+    inFile.close();
 }
 
 // Function calls function to add transactions to queue
 // Calls helper functions to processes transactions
 void Bank::processTransactions(const string& fileName) {
-  // call helper funciton to add and create structs
-  addToQueue(fileName); 
-  // transClassification();
-}
-
-// process file's lines as transactions and add to queue
-// throws invalid_argument exception if fileName not found
-void Bank::addToQueue(const string& fileName) {
-  // create file reader 
-  ifstream inFile;
-  inFile.open(fileName);
-   
-  if(!inFile) {
-    throw invalid_argument("Invalid file name!");
-  }
-
-  string line;
-  char type; 
-  string lastName;
-  string firstName;
-  int acctNum;
-  int acctNum2;
-  int amt;
-  int fund;
-  int fund2;
-
-
-  // while not end of file
-  // add transactions to queue
-  while(!inFile.eof()) {
-    getline(inFile, line);//check first letter 
-    stringstream s(line);    
-    s >> type; 
-    
-    switch(type) {
-      case 'O': // open account
-        s >> lastName >> firstName >> acctNum;
-
-        Action newAcct = {.transType = type, .last = lastName, .first = firstName, .acctTo = acctNum};
-
-        transactionsQ.push(newAcct);
-        break;
-      case 'D': // deposit
-        s >> acctNum >> amt;
-        fund = acctNum % 10; 
-        acctNum = acctNum /10;
-
-        Action deposit = {.transType = type, .acctTo = acctNum, .amount = amt, .fundTo = fund};
-
-        transactionsQ.push(deposit);
-        break;
-      case 'W': // withdraw
-        s >> acctNum >> amt;
-        fund = acctNum % 10; 
-        acctNum = acctNum /10;
-
-        Action withdraw = {.transType = type, .acctFrom = acctNum, .amount = amt, .fundFrom = fund};
-        
-        transactionsQ.push(withdraw);
-        break;
-      case 'T': // transfer
-        s >> acctNum >> amt >> acctNum2; 
-        fund = acctNum % 10; 
-        acctNum = acctNum /10;
-        fund2 = acctNum2 % 10; 
-        acctNum2 = acctNum2 /10;
-
-        Action transfer = {.transType = type, .acctTo = acctNum, .acctFrom = acctNum2, .amount = amt, .fundFrom = fund, .fundTo = fund2};
-
-        transactionsQ.push(transfer);
-        break;
-      case 'H': // display history
-        s >> acctNum; 
-        if(acctNum > 9999){ // display history of specific fund
-          fund = acctNum % 10; 
-          acctNum = acctNum /10;
-        } else { // display history of whole account
-          fund = -1; // no fund specified
+    // call helper funciton to add and create structs
+    addToQueue(fileName);
+    // process the queue
+    Account* hold; // holds the account searched for, if found
+    while (!transactionsQ.empty()) {
+        Action* tracker = &transactionsQ.front(); // holds first item in queue
+        switch (tracker->TransType) {
+        case 'O': { // open account
+            Account* openNew =
+                new Account(tracker->First, tracker->Last, tracker->AcctTo);
+            if (accounts.retrieve(tracker->AcctTo, hold)) {
+                // if the account number is already in use
+                cout << "Error, account ID already in use.";
+                break;
+            }
+            accounts.insert(openNew);
+            break;
         }
-        Action history = {.acctTo = acctNum, .fundFrom = .fund};
-        
-        transactionsQ.push(history);
-        break;
-    }
-  }
-
-  inFile.close();
+        case 'D': { // deposit
+            if (accounts.retrieve(tracker->AcctTo, hold)) {
+                // make the deposit and record the transaction
+                hold->deposit(tracker->Amount, tracker->FundTo);
+                string recordD;
+                recordD += tracker->TransType;
+                recordD += " " + to_string(tracker->AcctTo) +
+                           to_string(tracker->FundTo) + " " +
+                           to_string(tracker->Amount);
+                hold->recordTrans(recordD, tracker->FundTo);
+            } else { // if account not found
+                cout << "Could not locate account with client ID "
+                     << tracker->AcctTo << "." << endl;
+            }
+            break;
+        }
+        // withdraw
+        case 'W': {
+            if (accounts.retrieve(tracker->AcctFrom, hold)) {
+                if (hold->withdraw(tracker->Amount, tracker->FundFrom)) {
+                    // make the withdrawal and record the transaction
+                    string recordW;
+                    recordW += tracker->TransType;
+                    recordW += " " + to_string(tracker->AcctFrom) +
+                               to_string(tracker->FundFrom) + " " +
+                               to_string(tracker->Amount);
+                    hold->recordTrans(recordW, tracker->FundFrom);
+                    ;
+                } else
+                    cout << "Error, attempting to take out more than "
+                            "available balance."
+                         << endl;
+            } else {
+                // print error in transaction history of no account
+                cout << "Could not locate account with client ID "
+                     << tracker->AcctTo << "." << endl;
+            }
+            break;
+        }
+        // transfer
+        case 'T': {
+            Account* hold2; // pointer to hold the account to transfer to
+            // if both accounts exist
+            if (accounts.retrieve(tracker->AcctFrom, hold) &&
+                accounts.retrieve(tracker->AcctTo, hold2)) {
+                if (hold->withdraw(tracker->Amount, tracker->FundFrom)) {
+                    // make the withdrawal and record the transaction
+                    string recordW;
+                    recordW += tracker->TransType;
+                    recordW += " " + to_string(tracker->AcctFrom) +
+                               to_string(tracker->FundFrom) + " " +
+                               to_string(tracker->Amount) + " " +
+                               to_string(tracker->AcctTo) +
+                               to_string(tracker->FundTo);
+                    hold->recordTrans(recordW, tracker->FundFrom);
+                    ;
+                    // make the desposit and record the transaction
+                    hold2->deposit(tracker->Amount, tracker->FundTo);
+                    string recordD = to_string(tracker->TransType) + " " +
+                                     to_string(tracker->AcctTo) +
+                                     to_string(tracker->FundTo) + " " +
+                                     to_string(tracker->Amount);
+                    hold2->recordTrans(recordD, tracker->FundTo);
+                } else
+                    cout << "Error, attempting to take out more than "
+                            "available balance."
+                         << endl;
+            } else {
+                cout << "Could not locate account of one or more of the "
+                        "client IDs."
+                     << endl;
+            }
+            break;
+        }
+        // print history
+        case 'H': {
+            if (accounts.retrieve(tracker->AcctFrom, hold)) {
+                hold->displayTrans(tracker->FundFrom);
+            } else {
+                cout << "Could not locate account with client ID "
+                     << tracker->AcctFrom << "." << endl;
+            }
+            break;
+        }
+        default: {
+            cout << "Invalid transaction type.";
+            break;
+        }
+        } // end of switch
+        // pop action from queue
+        transactionsQ.pop();
+    } // end of while
 }
 
-// helper function that executes transactions, uses switch case to decide 
-// reminder to delete the structs after taking them from queue
-void Bank::processQueue(){
-  bool check = false; 
-  // create account pointer 
-  Account* account = nullptr; 
-  Account* account2 = nullptr; 
-  string first,lastname; 
-  int ID, amt, fundNum, ID2, fundNum2; 
-  Funds fundType, fundType2; 
-  // keeps track of errors 
-  bool success = false; 
-   // start switch case
-  switch(type){ 
-    case 'O': // open new account
-      break;
-    case 'D': // deposit
-      break; 
-    case 'W': // withdraw
-      break; 
-    case 'T': // transfer
-      break;  
-    case 'H': // display transaction history
-      
-      break;  
-    default: // incorrect transaction type
-      cout << "Improper transaction type" << endl;
-      break;  
-  }
-}
-
-// displays accounts' fund balances
-void Bank::displayAllBankBalances() const {}
-
-
-/*
-// helper function that executes transactions, uses switch case to decide 
-// reminder to delete the structs after taking them from queue
-void Bank::processQueue(){
-  bool check = false; 
-  // create account pointer 
-  Account* account = nullptr; 
-  Account* account2 = nullptr; 
-  string first,lastname; 
-  int ID, amt, fundNum, ID2, fundNum2; 
-  Funds fundType, fundType2; 
-  // keeps track of errors 
-  bool success = false; 
-   // start switch case
-  switch(type){ 
-    // open new account
-    case 'O':
-      break;
-    // deposit 
-    case 'D': 
-      
-      success = account->deposit(account,amt,fundType); 
-      break; 
-    // withdraw
-    case 'W':
-      s >> ID >> amt; 
-      //set fundType
-      fundNum = ID % 10; 
-      ID = ID/10; 
-      success = account->withdraw(amt,fundType);
-      break; 
-    // transfer
-    case 'T':
-      s >> ID >> amt >> ID2; 
-      //set fundType
-      fundNum = ID % 10; 
-      ID = ID/10;
-      fundNum2 = ID2 % 10;  
-      ID2 = ID2/10; 
-      success = account->transfer(account2,amt,fundType,fundType2); 
-      break;  
-    // history 
-    case 'H':
-      
-      break;  
-    default: // incorrect transaction type
-      cout << "Improper transaction type" << endl;
-      break;  
-  }
-}
-
-//checks account ID validation 
-bool Bank::checkData(stringstream& s,int& amt, int& fund, int& ID, int& ID2, int& fund2){
-   s >> ID >> amt; 
-   if(ID > 99999){
-      return false; 
-   }
-   fund = ID % 10; 
-   ID = ID/10; 
-   //if stream is not done yet 
-   if(!s.eof()){
-     s >> ID2; 
-     fund2 = ID2 % 10; 
-     ID2 = ID2/10; 
-   }
-  return true; 
-}
-
-
-//helper function to execute transcations 
-void Bank::transClassification(){
-  struct Action; 
-  while(!transactionsQ.empty()){
-      //set value of front in queue to string
-      Action = {transactionsQ.front()}; 
-      //delete front from queue 
-      transactionsQ.pop(); 
-      //take the first character of line
-      stringstream s(line);
-      s >> transactionType; 
-      processHelper(transactionType,line, s); 
-  }
-}
-*/
+// displays tree accounts' fund balances
+void Bank::displayAllBankBalances() const { accounts.display(); }
